8ad32e346d65debf877c975a839c8bd2976bd4e1
Subramanya <shashi-kiran.subramanya@hpe.com>
2018-12-06 16:28:03 +0530

    graphql done

--- src/api/healthcheck/healthcheck.js
+++ src/api/healthcheck/healthcheck.js
@@ -2,8 +2,37 @@
 
 const Handlers = require('./healthcheck-handlers.js');
 const routes = [];
+const GraphQL  = require('hapi-graphql');
+const  {GraphQLSchema, GraphQLObjectType, GraphQLString } =  require('graphql');
+
+const TestSchema = new GraphQLSchema({});
+
+// const { apolloHapi, graphiqlHapi } = require('apollo-server');
 
 exports.register = function (server, options, next) {
+  server.register({
+    register: GraphQL,
+    options: {
+      schema: TestSchema,
+      query: new GraphQLObjectType({
+        name: 'Query',
+        fields: {
+          author: {
+            type: GraphQLString,
+            resolve: (source, args, context, info) => {
+              return 'Thomas Reggi'
+            }
+          }
+        }
+      }),
+      route: {
+        path: '/graphql',
+        config: {}
+      }
+    }
+  }).catch((err)=>{
+    debugger
+  })
   routes.forEach(route => server.route(route));
   next();
 };
